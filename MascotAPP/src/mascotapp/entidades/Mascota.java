0466f5c39c3394d270a98e35c2a149f4d14b605c
Nico Santos <santosnicolas@outlook.com>
2023-03-29 18:03:42 -0300

    Act 28marz

---
+++ MascotAPP/src/mascotapp/entidades/Mascota.java
@@ -0,0 +1,92 @@
+package mascotapp.entidades;
+
+public class Mascota {
+    
+    private String nombre;
+    private String apodo;
+    // Conejo, Gato, Perro, Loro, Carpincho
+    private String tipo;
+    private String color;
+    private int edad;
+    private boolean cola;
+    private String raza;
+    
+    public Mascota(){
+        
+    }
+    
+    public Mascota(String nombre, String apodo, String tipo){
+        this.nombre = nombre;
+        this.apodo = apodo;
+        this.tipo = tipo;
+    }
+
+    public Mascota(String nombre, String apodo, String tipo, String color, int edad, boolean cola, String raza) {
+        this.nombre = nombre;
+        this.apodo = apodo;
+        this.tipo = tipo;
+        this.color = color;
+        this.edad = edad;
+        this.cola = cola;
+        this.raza = raza;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setCola(boolean cola) {
+        this.cola = cola;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public boolean isCola() {
+        return cola;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    
+    
+}
