c0f137e6f966957fa02a818a4e9cd4d2ae8f75ff
Nico Santos <santosnicolas@outlook.com>
2023-04-10 19:05:06 -0300

    guia 7 ejercicio juego terminado

--- Guia7/src/Entidades/Juego.java
+++ Guia7/src/Entidades/Juego.java
@@ -1,12 +1,57 @@
 package Entidades;
 
+import java.util.Scanner;
+
 public class Juego {
 
     private int numOculto;
     private int numAdivina;
     private int intentos;
+    private boolean ganado;
+    private boolean perdido;
+
+    public boolean isGanado() {
+        return ganado;
+    }
+
+    public void setGanado(boolean ganado) {
+        this.ganado = ganado;
+    }
+
+    public boolean isPerdido() {
+        return perdido;
+    }
+
+    public void setPerdido(boolean perdido) {
+        this.perdido = perdido;
+    }
+
+    Scanner leer = new Scanner(System.in);
 
     public void iniciar_juego() {
-        
+        System.out.println("JUGADOR 1\n----------");
+        System.out.println("Escriba el número para adivinar");
+        numOculto = leer.nextInt();
+        System.out.println("En cuántos intentos:");
+        intentos = leer.nextInt();
+        System.out.println("JUGADOR 2\n----------");
+        do {
+            System.out.println("Intenta adivinar el número, tiene " + intentos + " intentos");
+            numAdivina = leer.nextInt();
+            if (numAdivina < numOculto) {
+                intentos--;
+                System.out.println("Muy bajo, te quedan " + intentos + " intentos");
+            } else if (numAdivina > numOculto) {
+                intentos--;
+                System.out.println("Muy alto, te quedan " + intentos + " intentos");
+            } else if (numAdivina == numOculto) {
+                System.out.println("Adivinaste!");
+                ganado = true;
+            }
+        } while (intentos != 0 && ganado != true);
+        if (intentos == 0) {
+            perdido = true;
+        }
+
     }
 }
--- Guia7/src/G7ejercicioExtra3.java
+++ Guia7/src/G7ejercicioExtra3.java
@@ -1,8 +1,33 @@
 
+import Entidades.Juego;
+import java.util.Scanner;
+
 public class G7ejercicioExtra3 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+
+        Juego juego1 = new Juego();
+
+        String opc = null;
+        int gana1 = 0;
+        int gana2 = 0;
+        
+        System.out.println("ADIVINA EL NUMERO");
+
+        do {
+            juego1.iniciar_juego();
+            if (juego1.isGanado()) {
+                gana1 ++;
+            } else if (juego1.isPerdido()) {
+                gana2 ++;
+            }
+            System.out.println("Desea seguir jugando? s/n");
+            opc = leer.next();
+        } while (opc.equalsIgnoreCase("s"));
+        
+        System.out.println("JUGADOR 1 = " + gana1);
+        System.out.println("JUGADOR 2 = " + gana2);
     }
-    
+
 }
